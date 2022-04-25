064241037f7757776680d40a71d81acfae7cbbfe
Cindy Vuong <vuongcindytv@gmail.com>
2022-04-25 14:27:29 -0700

    dog_spec passed all rspecs

--- W2D1/dog_project/lib/dog.rb
+++ W2D1/dog_project/lib/dog.rb
@@ -1,3 +1,46 @@
 class Dog
-rf
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age 
+    end
+
+    def age= (number)
+        @age = number
+    end
+
+    def bark
+        if @age > 3
+            return @bark.upcase
+        else
+            return @bark.downcase
+        end
+    end
+
+    def favorite_foods
+        @favorite_foods
+    end
+
+    def favorite_food?(food_item)
+        favorite_foods_str = @favorite_foods.to_s
+        if favorite_foods_str.downcase.include?(food_item.downcase)
+            return true
+        else
+            return false
+        end
+    end
 end
