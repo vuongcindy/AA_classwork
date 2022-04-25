bbe8f7773b637a5f923b7e9d4b462404ca576077
Cindy Vuong <vuongcindytv@gmail.com>
2022-04-25 15:05:51 -0700

    bootcamp_project part 1 rspecs pass

--- W2D1/bootcamp_project/lib/bootcamp.rb
+++ W2D1/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,60 @@
+require 'byebug'
 class Bootcamp
-  
+  # Part 1
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |h, k| h[k] = []}
+
+    def name
+      @name
+    end
+
+    def slogan
+      @slogan
+    end
+
+    def student_capacity
+      @student_capacity
+    end
+
+    def teachers
+      @teachers
+    end
+
+    def students
+      @students
+    end
+
+    def grades
+      @grades 
+    end
+
+    def hire(teacher)
+      @teachers << teacher
+    end
+
+    def enroll(student)
+      if @students.length < @student_capacity
+        @students << student
+        return true
+      else
+        return false
+      end
+    end
+
+    def enrolled?(student)
+      if @students.include?(student)
+        return true
+      else
+        return false
+      end
+    end
+
+    # Part 2
+    
+  end
 end
