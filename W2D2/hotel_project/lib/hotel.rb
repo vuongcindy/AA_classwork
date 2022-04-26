4318ecef8ebbd0d54060196a0ea7453e1c6a438b
Cindy Vuong <vuongcindytv@gmail.com>
2022-04-26 16:36:46 -0700

    passes all specs

--- W2D2/hotel_project/lib/hotel.rb
+++ W2D2/hotel_project/lib/hotel.rb
@@ -1,5 +1,60 @@
 require_relative "room"
+require 'byebug'
 
 class Hotel
-  
+  def initialize(name, room_name_capacity)
+    @name = name
+    #hash keys are room names from hash arg, values are room instances with specified capacities
+    # @rooms = Hash.new { |h, k| h[k] = [] }
+    @rooms = {}
+    room_name_capacity.each do |k, v|
+        @rooms[k] = Room.new(v)
+    end
+  end
+
+  def name 
+    capitalized_name = []
+    name_parts = @name.split # arr of name parts
+    name_parts.each do |word|
+        capitalized_name << word.capitalize!
+    end
+    capitalized_name.join(" ")
+  end
+
+  def rooms
+    @rooms
+  end
+
+  def room_exists?(name)
+    rooms.has_key?(name)
+  end
+
+  def check_in(person, room_name)
+    if self.room_exists?(room_name)
+      if rooms[room_name].add_occupant(person)
+        puts 'check in successful'
+      else
+        puts 'sorry, room is full'
+      end
+    else
+      puts 'sorry, room does not exist'
+    end
+  end
+
+  def has_vacancy?
+    rooms.each do |room_name, max_capacity|
+      if max_capacity.available_space > 0
+        return true
+      end
+    end
+    false
+  end
+
+  def list_rooms
+    rooms.each do |room_name, max_capacity|
+      # debugger
+      puts "#{room_name} : #{max_capacity.available_space}"
+    end
+  end
+
 end
--- W2D2/hotel_project/lib/room.rb
+++ W2D2/hotel_project/lib/room.rb
@@ -1,3 +1,35 @@
 class Room
-  
+  def initialize(num)
+    @capacity = num
+    @occupants = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    if occupants.length < capacity
+        return false
+    elsif occupants.length == capacity
+        return true
+    end
+  end
+
+  def available_space
+    return capacity - occupants.length
+  end
+
+  def add_occupant(name)
+    if !full?
+        occupants << name
+        return true
+    else
+        return false
+    end
+  end
 end
